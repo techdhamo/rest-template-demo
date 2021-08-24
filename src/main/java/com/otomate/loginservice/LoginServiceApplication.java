003b9bd386b27af9301488b6192c34da829e3a2c
dhamo <dhamo@ASUS-TUF-A15>
2021-08-24 10:37:19 +0530

    Initial Commit:

--- src/main/java/com/otomate/loginservice/LoginServiceApplication.java
+++ src/main/java/com/otomate/loginservice/LoginServiceApplication.java
@@ -2,12 +2,19 @@ package com.otomate.loginservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class } )
 public class LoginServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginServiceApplication.class, args);
+	} 
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
